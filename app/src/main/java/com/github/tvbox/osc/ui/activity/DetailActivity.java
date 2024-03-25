3019765095d38c750a7ad62b01efa303a6d7b7d2
zhanxy <766025982@qq.com>
2024-03-25 20:55:45 +0800

    Revert "二级页阿里链接自动跳转"
    This reverts commit 29cf5b66c93f2322e71e06a8dcb15f3b62daa5ad.

--- app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/DetailActivity.java
@@ -122,7 +122,6 @@ public class DetailActivity extends BaseActivity {
     private SeriesAdapter seriesAdapter;
     public String vodId;
     public String sourceKey;
-    public String firstsourceKey;
     boolean seriesSelect = false;
     private View seriesFlagFocus = null;
     private HashMap<String, String> mCheckSources = null;
@@ -207,7 +206,7 @@ public class DetailActivity extends BaseActivity {
                     if (vodInfo.seriesMap.get(vodInfo.playFlag).size() > vodInfo.playIndex) {
                         vodInfo.seriesMap.get(vodInfo.playFlag).get(vodInfo.playIndex).selected = true;
                     }
-                    insertVod(firstsourceKey, vodInfo);
+                    insertVod(sourceKey, vodInfo);
                     seriesAdapter.notifyDataSetChanged();
                 }
             }
@@ -404,7 +403,7 @@ public class DetailActivity extends BaseActivity {
             preFlag = vodInfo.playFlag;
             Bundle bundle = new Bundle();
             //保存历史
-            insertVod(firstsourceKey, vodInfo);
+            insertVod(sourceKey, vodInfo);
             bundle.putString("sourceKey", sourceKey);
             bundle.putSerializable("VodInfo", vodInfo);
             if (showPreview) {
@@ -502,23 +501,16 @@ public class DetailActivity extends BaseActivity {
                 if (absXml != null && absXml.movie != null && absXml.movie.videoList != null && absXml.movie.videoList.size() > 0) {
                     showSuccess();
                     mVideo = absXml.movie.videoList.get(0);
-                    mVideo.id = vodId;
-                    if (TextUtils.isEmpty(mVideo.name)) mVideo.name = "片名";
                     vodInfo = new VodInfo();
                     vodInfo.setVideo(mVideo);
                     vodInfo.sourceKey = mVideo.sourceKey;
-                    sourceKey = mVideo.sourceKey;
 
-                    tvName.setText(mVideo.name);                    
-                    setTextShow(tvSite, getString(R.string.det_source), ApiConfig.get().getSource(firstsourceKey).getName());
+                    tvName.setText(mVideo.name);
+                    setTextShow(tvSite, getString(R.string.det_source), ApiConfig.get().getSource(mVideo.sourceKey).getName());
                     setTextShow(tvYear, getString(R.string.det_year), mVideo.year == 0 ? "" : String.valueOf(mVideo.year));
                     setTextShow(tvArea, getString(R.string.det_area), mVideo.area);
-                    setTextShow(tvLang, getString(R.string.det_lang), mVideo.lang);              
-                    if (!firstsourceKey.equals(sourceKey)) {
-                    	setTextShow(tvType, getString(R.string.det_type), "[" + ApiConfig.get().getSource(sourceKey).getName() + "] 解析");
-                    } else {
-                    	setTextShow(tvType, getString(R.string.det_type), mVideo.type);
-                    }
+                    setTextShow(tvLang, getString(R.string.det_lang), mVideo.lang);
+                    setTextShow(tvType, getString(R.string.det_type), mVideo.type);
                     setTextShow(tvActor, getString(R.string.det_actor), mVideo.actor);
                     setTextShow(tvDirector, getString(R.string.det_dir), mVideo.director);
                     setTextShow(tvDes, getString(R.string.det_des), removeHtmlTag(mVideo.des));
@@ -622,7 +614,6 @@ public class DetailActivity extends BaseActivity {
         if (vid != null) {
             vodId = vid;
             sourceKey = key;
-            firstsourceKey = key;
             showLoading();
             sourceViewModel.getDetail(sourceKey, vodId);
 
@@ -649,12 +640,12 @@ public class DetailActivity extends BaseActivity {
                         mGridView.setSelection(index);
                         vodInfo.playIndex = index;
                         //保存历史
-                        insertVod(firstsourceKey, vodInfo);
+                        insertVod(sourceKey, vodInfo);
                     }
                 } else if (event.obj instanceof JSONObject) {
                     vodInfo.playerCfg = event.obj.toString();
                     //保存历史
-                    insertVod(firstsourceKey, vodInfo);
+                    insertVod(sourceKey, vodInfo);
                 }
 
             }
